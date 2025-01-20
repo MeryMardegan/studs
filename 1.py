5bcba31150791371a93362d23fbd404dd8737d3b
MeryMardegan <marymardegan@usp.br>
2025-01-20 14:34:44 -0300

    Changes to be committed:

--- 1.py
+++ 1.py
@@ -9,7 +9,10 @@ if nome and idade:
     print (f'Seu nome é {nome}')
     print (f'Sua idade é {idade}')
     print (f'Seu nome invertido é {nome[::-1]}')
-    print (f'Seu nome contém espaço: {" " in nome}')
+    if ' ' in nome:
+        print ('Seu nome contém espaço')
+    else:
+        print ('Seu nome não contém espaço')
     print (f'Seu nome tem {len(nome)} letras')
     print (f'A primeira letra do seu nome é {nome[0]}')
     print (f'A última letra do seu nome é {nome[-1]}')
