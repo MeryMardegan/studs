06980fc5bb1e19376d15d5fd2b555d2e5a926400
MeryMardegan <marymardegan@usp.br>
2025-01-20 14:21:58 -0300

    new file:   1.py

---
+++ 1.py
@@ -0,0 +1,19 @@
+'''
+# Peça para o usuário digitar seu nome
+nome = input('Digite seu nome: ')
+# Peça para o usuário digitar sua idade
+idade = input('Digite sua idade: ')
+
+Se o nome e a idade forem digitados:
+    Exiba:
+        Seu nome é {nome}
+        Seu nome invertido é {nome invertido}
+        Seu nome contém (ou não) espaço
+        Seu nome tem {n} letras
+        A primeira letra do seu nome é {primeira letra}
+        A última letra do seu nome é {última letra}
+Se nada for digitado em nome ou idade:
+    Exiba:
+        Nada foi digitado, tente novamente
+
+    '''
